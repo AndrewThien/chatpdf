9db62c2f668e415fe1d334c925ab8a8297e73210
Tri Thien Nguyen <anretrithien@gmail.com>
2023-11-20 11:48:43 +0000

    Fix 2

--- src/lib/s3-server.ts
+++ src/lib/s3-server.ts
@@ -1,37 +1,53 @@
-import { createWriteStream } from 'fs';
+import { S3 } from "@aws-sdk/client-s3";
+import fs from "fs";
+import path from "path";
+import stream from "stream"; // Import stream module
 import { join } from 'path';
-import { tmpdir } from 'os';  
-import AWS from 'aws-sdk';
+import { tmpdir } from 'os'; 
 
-export async function downloadFromS3(fileKey: string) {
+export async function downloadFromS3(file_key: string): Promise<string> {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const s3 = new S3({
+        region: "eu-west-2",
+        credentials: {
+          accessKeyId: process.env.NEXT_PUBLIC_S3_ACCESS_KEY_ID!,
+          secretAccessKey: process.env.NEXT_PUBLIC_S3_SECRET_ACCESS_KEY!,
+        },
+      });
+      const params = {
+        Bucket: process.env.NEXT_PUBLIC_S3_BUCKET_NAME!,
+        Key: file_key,
+      };
 
-  try {
+      const obj = await s3.getObject(params);
 
-    AWS.config.update({
-      accessKeyId: process.env.NEXT_PUBLIC_S3_ACCESS_KEY_ID,
-      secretAccessKey: process.env.NEXT_PUBLIC_S3_SECRET_ACCESS_KEY 
-    });
+      let readableStream: stream.Readable;
 
-    const s3 = new AWS.S3({
-      params: { Bucket: process.env.NEXT_PUBLIC_S3_BUCKET_NAME }, 
-      region: 'eu-west-2'
-    });
+      if (obj.Body instanceof Buffer) {
+        // If obj.Body is a Buffer, convert it to a readable stream
+        readableStream = stream.Readable.from([obj.Body]);
+      } else if (obj.Body instanceof stream.Readable) {
+        // If obj.Body is already a readable stream, use it directly
+        readableStream = obj.Body;
+      } else {
+        reject(new Error("Object body is not a Readable stream"));
+        return;
+      }
 
-    const obj = await s3.getObject({ Key: fileKey }).promise();
+      const file_name = join(tmpdir(), `${fileKey}-${Date.now()}.pdf`);
 
-    const fileName = join(tmpdir(), `${fileKey}-${Date.now()}.pdf`);
+      const file = fs.createWriteStream(file_name); // Create a write stream for the file
 
-    const stream = createWriteStream(fileName);
-
-    stream.write(obj.Body); 
-
-    stream.end();
-
-    return fileName;
-
-  } catch (error) {
-   console.error(error);
-   return null;
-  }
-
+      file.on("open", function (_fd) {
+        readableStream.pipe(file).on("finish", () => {
+          resolve(file_name);
+        });
+      });
+    } catch (error) {
+      console.error(error);
+      reject(error);
+    }
+  });
 }
+
